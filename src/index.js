618f546072fc59b751b60c4327fa7ac073d9f39c
Dm3583 <pankovdm@gmail.com>
2020-12-16 16:37:20 +0200

    uodate

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import './styles.css';
-import './js/timer';
+import { CountdownTimer } from './js/timer';
+import { refs } from './js/refs';
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let date = 'Dec 31, 2020 23:59:59';
+
+const timer = new CountdownTimer({
+    selector: refs.timerId,
+    targetDate: new Date(date),
+});
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let date2 = 'Dec 29, 2018 10:00';
+
+const timer2 = new CountdownTimer({
+    selector: refs.timerId2,
+    targetDate: new Date(date2),
+});
--- src/js/timer.js
+++ src/js/timer.js
@@ -1,6 +1,4 @@
-import { refs } from './refs';
-
-class CountdownTimer {
+export class CountdownTimer {
     constructor(setupObj) {
         this.setupObj = setupObj;
         this.intervalId = null;
@@ -58,22 +56,4 @@ class CountdownTimer {
     };
 }
 
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-let date = 'Dec 31, 2020 23:59:59';
-
-const timer = new CountdownTimer({
-    selector: refs.timerId,
-    targetDate: new Date(date),
-});
-
-
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-let date2 = 'Dec 29, 2018 10:00';
-
-const timer2 = new CountdownTimer({
-    selector: refs.timerId2,
-    targetDate: new Date(date2),
-});
